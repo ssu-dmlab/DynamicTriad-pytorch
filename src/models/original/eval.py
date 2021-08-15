25ac9b93f03a13a32820f2f4b9c07e6896c6e38c
jong whi lee <whdgnl1123@gmail.com>
2021-08-15 15:34:09 +0900

    implement change link evaluation tasks

--- src/models/original/eval.py
+++ src/models/original/eval.py
@@ -11,6 +11,10 @@ class Evaluator:
 			self.evaluate = self.evaluate_link_reconstruction
 		elif mode == 'link_prediction':
 			self.evaluate = self.evaluate_link_prediction
+		elif mode == 'change_link_reconstruction':
+			self.evaluate = self.evaluate_change_link_reconstruction
+		elif mode == 'change_link_prediction':
+			self.evaluate = self.evaluate_change_link_prediction
 		else:
 			raise RuntimeError("Unkonwn task: {}".format(mode))
 
@@ -23,7 +27,7 @@ class Evaluator:
 			features = torch.abs(u1 - u2).cpu()
 
 		val_scores = []
-		cv = StratifiedKFold(n_splits=2, shuffle=True)
+		cv = StratifiedKFold(n_splits=5, shuffle=True)
 		parts = cv.split(features, labels)
 
 		for train, test in parts:
@@ -79,3 +83,60 @@ class Evaluator:
 
 	def evaluate_link_prediction(self, model, dataset):
 		return self.evaluate_link_reconstruction(model, dataset, interval=1)
+
+	def evaluate_change_link_reconstruction(self, model, dataset, interval=0):
+		samples, labels = self.sample_change_link_reconstruction(dataset, model.timesteps, interval=interval)
+
+		with torch.no_grad():
+			u1 = model.embedding[samples[:, 0] - interval, samples[:, 1]]
+			u2 = model.embedding[samples[:, 0] - interval, samples[:, 2]]
+			features = torch.abs(u1 - u2).cpu()
+
+		val_scores = []
+		cv = StratifiedKFold(n_splits=5, shuffle=True)
+		parts = cv.split(features, labels)
+
+		for train, test in parts:
+			classifier = LogisticRegression()
+			classifier = classifier.fit(features[train], labels[train])
+			predict = classifier.predict(features[test])
+			val_scores.append(f1_score(labels[test], predict))
+
+		return np.mean(val_scores)
+
+	def sample_change_link_reconstruction(self, dataset, timesteps, interval=0):
+		prev_graphs = dataset[interval:len(dataset)-1]
+		current_graphs = dataset[interval+1:]
+
+		samples, labels = [], []
+
+		for i, (prev_graph, current_graph) in enumerate(zip(prev_graphs, current_graphs)):
+			if i >= timesteps-1:
+				break
+
+			prev_edges = set()
+			for e0, e1 in prev_graph.edges():
+				if not (e0, e1) in prev_edges:
+					prev_edges.add((e0, e1))
+
+			current_edges = set()
+			for e0, e1 in current_graph.edges():
+				if not (e0, e1) in current_edges:
+					current_edges.add((e0, e1))
+
+			for e0, e1 in current_edges - prev_edges:
+				e0 = dataset.vertex2index[e0]
+				e1 = dataset.vertex2index[e1]
+				samples.append([i+1, e0, e1])
+				labels.append(1)
+
+			for e0, e1 in prev_edges - current_edges:
+				e0 = dataset.vertex2index[e0]
+				e1 = dataset.vertex2index[e1]
+				samples.append([i+1, e0, e1])
+				labels.append(0)
+
+		return np.array(samples), np.array(labels)
+
+	def evaluate_change_link_prediction(self, model, dataset):
+		return self.evaluate_change_link_reconstruction(model, dataset, interval=1)
